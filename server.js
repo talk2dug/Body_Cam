2388f96ff10e21e41a08224ae7b88c9311576b78
Jack Swayze <talk2dug@gmail.com>
2020-09-10 14:34:49 -0400

    added GPS functions

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var mainServer = require('socket.io-client')('http://192.168.196.163:3000');
 var moment = require('moment');
 var scp = require('scp');
+var geolib = require('geolib');
+var GPS = require('./node_modules/gps/gps.js');
 var fileNameTImeStamp;
 var name;
 
@@ -58,6 +60,77 @@ var spawn=require('child_process').spawn
     }
     });
 
-
+    var gps = new GPS;
+    const SerialPort = require('serialport')
+    const port = new SerialPort('/dev/ttyS0', {
+        baudRate: 9600
+    })
+    const Readline = require('@serialport/parser-readline')
+    
+    // Open errors will be emitted as an error event
+    port.on('error', function(err) {
+      console.log('Error: ', err.message)
+    })
+    
+    
+    
+    
+    
+    function parseGPS(data) {
+        var lat = data.state.lat;
+        var lon = data.state.lon;
+        //console.log(lat)
+        if (lat != null) {
+            tripA.push({latitude: lat,longitude: lon});
+            tripB.push({latitude: lat,longitude: lon})
+        }
+        if (tripA.length === 4) {
+            var distance = geolib.getPathLength(tripA, [10, 5]);
+            distance = geolib.convertUnit('mi', distance, 4)
+            distanceTripA = distanceTripA + distance;
+            io.emit('distanceA', distanceTripA);
+            tripA.length = 0;
+        }
+        if (tripB.length === 4) {
+            var distance = geolib.getPathLength(tripB, [10, 5]);
+            distance = geolib.convertUnit('mi', distance, 4)
+            distanceTripB = distanceTripB + distance;
+            io.emit('distanceB', distanceTripB);
+            tripB.length = 0;
+        }
+    }
+    const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+    parser.on('data', function(data) {
+        gps.update(data);
+        parseGPS(gps);
+    })
+    function calculateHeading(lon, lat) {
+        var Heading = 0;
+        var angles = require('angles');
+        Heading = GPS.Heading(prevLAT, prevLAT, lat, lon);
+        Heading = Heading.toFixed(0)
+        prevLAT = lat;
+        prevLON = lon;
+        return Heading;
+    }
+    gps.on('GGA', function(data) {
+        console.log(data)
+        var headingDir = calculateHeading(data.lon, data.lat)
+        GPSarray['lon'] = data.lon
+        GPSarray['lat'] = data.lat
+        GPSarray['heading'] = headingDir
+        if (gps.state.speed != null) {GPSarray['speed'] = gps.state.speed.toFixed(2)}
+        if (gps.state.speed == null) {GPSarray['speed'] = 0}
+            GPSarray['time'] = data.time
+            if (data.lon === null) {
+            var pos = {'lon': '-76.558225','lat': '38.06033333333333'}
+        } else {
+            var pos = {'lon': data.lon,'lat': data.lat}
+        }
+    
+        //mainServer.emit('gps', GPSarray)
+        //dreamHost.emit('gpsData', GPSarray)
+        //io.emit('state', GPSarray);
+    });
 
 
